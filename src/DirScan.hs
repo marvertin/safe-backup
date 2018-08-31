6c016701cc24890832708fbb69e2469d85cac2ad
Martin Veverka <martin.veverka@email.cz>
2018-08-31 23:03:03 +0200

    Reverzní cestu zavést

--- src/DirScan.hs
+++ src/DirScan.hs
@@ -32,38 +32,51 @@ import           Types
 type FlowAvar = [(UTCTime, Int, Integer)] -- timce, count, size, header has latest
 newtype Acum = Acum FlowAvar deriving (Show)
 
+type RevPath = [String] -- it is reverse list of path items: ["myfile.txt", "myaccount", "home", "opt"]
+data Fce a = Fce (RevPath -> [(FileName, a)] -> a) -- directory not creator
+                    (RevPath -> Bool)                 -- dir or file filter
+                    (RevPath -> IO a)                 -- file processor
+
 scanDirectory :: FilePath -> IO ()
-scanDirectory path = do
+scanDirectory rootPath = do
     putStrLn ""
     startTime <- getCurrentTime
-    Acum ((_, count, size): _) <- scanDirectory' 0  (Acum [(startTime, 0, 0), (startTime, 0, 0)]) path
+    Acum ((_, count, size): _) <- scanDirectory' 0  (Acum [(startTime, 0, 0), (startTime, 0, 0)]) []
     endTime <- getCurrentTime
     let (countSpeed, sizeSpeed) = averageSpeed' (startTime, 0, 0) (endTime, count, size)
     putStrLn "Total: "
     printf "%6d# %10.3f MB | %9.2f #/s  %10.3f MB/s  \n" count (sizeInMb size) countSpeed sizeSpeed
     putStrLn ""
+ where
+  scanDirectory' :: Int -> Acum -> RevPath -> IO Acum
+  scanDirectory' level acum@(Acum flowAvar@((time, count, size) : _)) revpath = do
+    let fullPath = rootPath </> pth revpath
+    -- putStrLn $ "Pokus: " ++ path
+    isDir <- doesDirectoryExist fullPath
+    -- time <- getCurrentTime
+    --putStr $  "\r" ++ show level ++ ": " ++ show acum ++ "  " ++ replicate (level * 4) ' ' ++ path
+    -- print time
+    -- printf "%2d #%6d %10.3f MB %s %s  \n" level count (sizeInMb size) (show time) path
+    if isDir then do
+        fords <- listDirectory fullPath -- simple names
+        -- fords <- fmap (fmap (path </>)) (listDirectory fullPath)
+        foldM (scanDirectory' (level + 1)) acum (fmap (:revpath) fords)
+     else do
+       -- sz <- getFileSize path
+       sz <- readAndCountBytes revpath
+       Acum <$> updateFlowAvar flowAvar (count + 1, size + fromIntegral sz)
 
-scanDirectory' :: Int -> Acum -> FilePath -> IO Acum
-scanDirectory' level acum@(Acum flowAvar@((time, count, size) : _)) path = do
-  -- putStrLn $ "Pokus: " ++ path
-  isDir <- doesDirectoryExist path
-  -- time <- getCurrentTime
-  --putStr $  "\r" ++ show level ++ ": " ++ show acum ++ "  " ++ replicate (level * 4) ' ' ++ path
-  -- print time
-  -- printf "%2d #%6d %10.3f MB %s %s  \n" level count (sizeInMb size) (show time) path
-  if isDir then do
-      fords <- fmap (fmap (path </>)) (listDirectory path)
-      foldM (scanDirectory' (level + 1)) acum fords
-   else do
-     -- sz <- getFileSize path
-     sz <- readAndCountBytes path
-     Acum <$> updateFlowAvar flowAvar (count + 1, size + fromIntegral sz)
+  readAndCountBytes :: RevPath -> IO Integer
+  readAndCountBytes revpath = do
+   dataa <- Lazy.readFile (fullPth revpath)
+   evaluate $ fromIntegral (Lazy.length dataa)
 
+  fullPth :: RevPath -> FilePath
+  fullPth p = rootPath </> pth  p
 
-readAndCountBytes :: FilePath -> IO Integer
-readAndCountBytes path = do
-  dataa <- Lazy.readFile path
-  evaluate $ fromIntegral (Lazy.length dataa)
+pth :: RevPath -> FilePath
+pth = foldl (flip (</>)) []
+
 
 
 
