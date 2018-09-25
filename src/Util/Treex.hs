85d20257fde3c75fe250636af79ee8156e919af6
Martin Veverka <martin.veverka@email.cz>
2018-09-25 20:27:34 +0200

    stromování

--- src/Util/Treex.hs
+++ src/Util/Treex.hs
@@ -1,6 +1,9 @@
 module Util.Treex (
 
- Tree(..)
+ Tree(..),
+ zipPath,
+ union,
+ intersection,
 ) where
 
 import qualified Data.Map    as M
@@ -12,7 +15,7 @@ instance Functor (Tree k) where
   fmap fce (Tree x ch) = Tree (fce x) (M.map (fmap fce) ch)
 
 zipPath :: Tree k v -> Tree k ([k], v)
-zipPath tree = zp [] tree
+zipPath = zp []
   where
     zp :: [k] -> Tree k v -> Tree k ([k], v)
     zp revpath (Tree x ch) =
@@ -26,5 +29,21 @@ instance Traversable (Tree k) where
   traverse fce (Tree x ch) = Tree <$> fce x <*> traverse (traverse fce) ch
 
 
-union :: Tree k a -> Tree k b -> Tree k (Maybe a, Maybe b)
-union (Tree x xch) (Tree y ych) = Tree (Just x, Just y) M.empty
+union :: Ord k => Tree k a -> Tree k b -> Tree k (Maybe a, Maybe b)
+union treeA treeB = uni (fmap (\a -> (Just a, Nothing)) treeA)
+                        (fmap (\b -> (Nothing, Just b)) treeB)
+  where
+    uni :: Ord k => Tree k (Maybe a, Maybe b) -> Tree k (Maybe a, Maybe b) -> Tree k (Maybe a, Maybe b)
+    uni (Tree (x, _) xch) (Tree (_, y) ych) = Tree (x, y) (M.unionWith uni xch ych)
+
+
+intersection :: Ord k => Tree k a -> Tree k b -> Tree k (a, b)
+intersection (Tree x xch) (Tree y ych) = Tree (x, y) (M.intersectionWith intersection xch ych)
+
+{-
+lookupx :: [k] -> Tree k a -> Maybe (Tree k a)
+lookupx [] tree      = Just tree
+lookupx (k: ks) (Tree _ ch) = case M.lookup ch of
+  Nothing      -> Nothing
+  (Just tree2) -> lookupx tree2
+-}
